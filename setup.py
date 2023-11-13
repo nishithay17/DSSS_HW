bebe6e023e9cac2e3d36d7fe17e4f316d61dfacd
Nishitha Yedalapalli <94312114+nishithay17@users.noreply.github.com>
2023-11-13 23:04:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='math_quiz',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add other dependencies here
+    ],
+    # Other metadata like author, author_email, description, etc.
+)
